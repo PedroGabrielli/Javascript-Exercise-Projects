d5bdf7d2432e930e4b5a2ab7a1fff6b7689543aa
unknown <97773@sistemas.frc.utn.edu.ar>
2025-02-27 00:14:39 -0300

    commit final

---
+++ main.js
@@ -0,0 +1,69 @@
+let btnPrev = document.querySelector(".btns__prev");
+let btnNext = document.querySelector(".btns__next");
+let progressBar = document.querySelector(".steps__bar");
+let stepsCircle = document.querySelectorAll(".steps__step");
+let progress = 0;
+let counter = 1;
+
+btnNext.addEventListener("click", () =>{
+    counter++;
+    progress += 25;
+    
+
+    if(counter > stepsCircle.length){
+        counter = stepsCircle.length;
+        progress = 100;
+        
+    }
+    
+    progressBar.style.height = progress+"%";
+    disableBtns(progress)
+    borderActive();
+});
+
+btnPrev.addEventListener("click", () =>{
+    counter--;
+    progress -= 25;
+    
+
+    if(counter < 1){
+        counter = 1;
+        progress = 0;
+        
+    }
+    
+    progressBar.style.height = progress+"%";
+    disableBtns(progress)
+    borderActive();
+});
+
+
+let disableBtns = (progress) => {
+    if(progress >=100){
+        btnNext.setAttribute("disabled", "true");
+        btnNext.classList.add("disable");
+    }else{
+        btnNext.removeAttribute("disabled", "true");
+        btnNext.classList.remove("disable");
+
+    }
+    if(progress <= 0){
+       btnPrev.setAttribute("disabled", "true");
+       btnPrev.classList.add("disable");
+    }else{
+        btnPrev.removeAttribute("disabled", "true");
+        btnPrev.classList.remove("disable")
+    }
+}
+
+let borderActive = () => {
+    stepsCircle.forEach((step, i) => {
+    if(counter > i){
+        step.classList.add("active");
+    }else{
+        step.classList.remove("active");
+    }
+
+    })
+}
+borderActive;
